262196e8ea88e58a1c5cb06a7dd53316ff86f317
fernandobouchet <fernandobouchet@gmail.com>
2024-12-15 20:17:28 -0300

    feat: update 'repository' to use new h2 database

--- src/main/java/com/fernandobouchet/runners/Application.java
+++ src/main/java/com/fernandobouchet/runners/Application.java
@@ -2,6 +2,7 @@ package com.fernandobouchet.runners;
 
 import com.fernandobouchet.runners.run.Location;
 import com.fernandobouchet.runners.run.Run;
+import com.fernandobouchet.runners.run.RunRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -15,17 +16,17 @@ import java.time.temporal.ChronoUnit;
 @SpringBootApplication
 public class Application {
 
-	private static final Logger log = LoggerFactory.getLogger(Application.class);
+    private static final Logger log = LoggerFactory.getLogger(Application.class);
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 
-	@Bean
-	CommandLineRunner runner() {
-			return args -> {
-			Run	run = new Run(1,"First Run",LocalDateTime.now(), LocalDateTime.now().plus(3, ChronoUnit.HOURS),3, Location.OUTDOOR);
-			log.info("Run:" + run);
-			};
-	}
+    @Bean
+    CommandLineRunner runner(RunRepository runRepository) {
+        return args -> {
+            Run run = new Run(6, "First Run", LocalDateTime.now(), LocalDateTime.now().plus(3, ChronoUnit.HOURS), 3, Location.OUTDOOR);
+            runRepository.create(run);
+        };
+    }
 }
